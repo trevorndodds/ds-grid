e37d713e56653585f7825ba29f56d9e4fdf3d748
trevor <trevor.dodds@gmail.com>
2016-12-06 21:08:00 -0500

    initial

---
+++ GridAppService/GridApp1Service.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DataSynapse.GridServer.Engine;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Diagnostics;
+
+namespace GridAppService
+{
+    class NetTrevTest
+    {
+        public NetTrevTest() { }
+
+        public Object add(double a, double b)
+        {
+            double result = a + b;
+            long taskid = EngineSession.GetServiceInvocationContext().InvocationId;
+            String engine = EngineSession.GetProperties().Get(EngineProperties.USERNAME) + "-" + EngineSession.GetProperties().Get(EngineProperties.INSTANCE);
+            String output = engine +" " + result;
+            return output;
+        }
+
+        public Object MonteCarloPi(int n)
+        {
+            int inside = 0;
+            Random r = new Random();
+
+            for (int i = 0; i < n; i++)
+            {
+                if (Math.Pow(r.NextDouble(), 2) + Math.Pow(r.NextDouble(), 2) <= 1)
+                {
+                    inside++;
+                }
+            }
+            double result =  4.0 * inside / n;
+            String engine = EngineSession.GetProperties().Get(EngineProperties.USERNAME) + "-" + EngineSession.GetProperties().Get(EngineProperties.INSTANCE);
+            String output = engine + " sample size:" + n + " " + result;
+
+            return output;
+        }
+
+        public Object calcPrime(int n)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            List<int> primes = new List<int>();
+            primes.Add(2);
+            int nextPrime = 3;
+            while (primes.Count < n)
+            {
+                int sqrt = (int)Math.Sqrt(nextPrime);
+                bool isPrime = true;
+                for (int i = 0; (int)primes[i] <= sqrt; i++)
+                {
+                    if (nextPrime % primes[i] == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime)
+                {
+                    primes.Add(nextPrime);
+                }
+                nextPrime += 2;
+            }
+
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+
+            String engine = EngineSession.GetProperties().Get(EngineProperties.USERNAME) + "-" + EngineSession.GetProperties().Get(EngineProperties.INSTANCE);
+            String output = engine + " Primes: " + primes.Count + " Time (s): " + ts.TotalSeconds;
+
+            return output;
+        }
+
+
+    }
+}
---
+++ gridApp1/GridApp1Client.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DataSynapse.GridServer.Client;
+using DataSynapse.GridServer.Driver;
+using DataSynapse.GridServer.Engine;
+using System.Collections;
+using System.Collections.Specialized;
+
+namespace gridApp1
+{
+    class GridApp1Client
+    {
+        static void Main(string[] args)
+        {
+            String service = "GridAppService";
+          //  String username = "admin";
+         //   String password = "admin";
+            String priority = "5";
+         //   int initPayload = 100;
+         //   String method = "MonteCarloPi";
+            String method = "calcPrime";
+            int tasks = 5;
+            string pdirector = "http://ds01.root.net:8000";
+
+
+            string svcName = "svcName";
+            string appName = "Grid App1";
+            string appDesc = "Trev Test";
+            NameValueCollection props = new NameValueCollection();
+            props.Add(Options.PRIORITY, priority);
+            
+            NameValueCollection dprops = new NameValueCollection();
+            dprops.Add(Description.SERVICE_NAME, svcName);
+            dprops.Add(Description.APP_DESCRIPTION, appDesc);
+            dprops.Add(Description.APP_NAME, appName);
+            dprops.Add(Description.DEPT_NAME, "Dept");
+            //DriverManager.SetProperty("DSUsername", "trevor");
+            //DriverManager.SetProperty("DSPassword", "");
+            DriverManager.SetProperty("DSLogLevel", "FINEST");
+            DriverManager.SetProperty("DSLogToSystem", "true");
+            DriverManager.SetProperty("DSNegotiateEnabled", "true");
+            DriverManager.SetProperty("DSPrimaryDirector", pdirector);
+            DriverManager.SetProperty("DSSecondaryDirector", pdirector);
+
+
+            responseHandler handler = new responseHandler();
+            try
+            {
+                //  Service a = ServiceFactory.GetInstance().CreateService("blah");
+                Service s = ServiceFactory.GetInstance().CreateService(service, null, props, dprops);
+                Random r = new Random();
+                for (long i = 0; i < tasks; i++)
+                {
+                    //  s.Submit(method, new Object[] { Convert.ToDouble(r.Next(0, 9000)), Convert.ToDouble(r.Next(0, 9000)) }, handler);
+                    s.Submit(method, 500000, handler);
+                }
+                s.WaitUntilInactive(0);
+
+                s.Destroy();
+            }
+            catch (Exception e)
+            {
+              //  Console.WriteLine("Error: {0}", e.Message);
+                Console.WriteLine("Error: {0}", e.InnerException);
+             //       throw;
+            }
+
+
+        }
+
+
+        public class responseHandler : ServiceInvocationHandler
+        {
+            private long handled = 0;
+
+            public void HandleError(ServiceInvocationException e, int id)
+            {
+                Console.WriteLine("Error from " + id + ": " + e.Message + " task count: " + (++handled));
+            }
+
+            public void HandleResponse(object response, int id)
+            {
+                Console.WriteLine((++handled) + " - Result for request #" + id + ", " + response);
+            }
+        }
+    }
+}
